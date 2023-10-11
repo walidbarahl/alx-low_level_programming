bc0a78bdc1d59b716634c6809141e67e3159441e
walidbarahl <ultramaroc01@gmail.com>
2023-10-11 15:45:25 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,10 @@ int print_last_digit(int n)
 	int lastDigit;
 
 	if (n < 0)
-		lasDigit = -1 * (n % 10);
+		lastDigit = -1 * (n % 10);
 	else
-		lasDigit = n % 10;
+		lastDigit = n % 10;
+
 	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
