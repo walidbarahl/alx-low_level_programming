80e369dbf1a70844d33ca4ae9caea43693d5a448
walidbarahl <ultramaroc01@gmail.com>
2023-10-11 17:09:36 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 {
 	int prod, nult, num;
 
-	if (n <= 15 && m >= 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (num = 0; num <= n; ++num)
 		{
@@ -30,12 +30,12 @@ void print_times_table(int n)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
-				} else if (prod <= 99 && prod <= 10)
+				} 
+				else if (prod <= 99 && prod <= 10)
 					_putchar((prod / 10) +48);
-				_putchar((pro % 10 + 48);
+				_putchar((prod % 10 + 48);
 						}
 						_putchar('\n');
 						}
 						}
 						}
-
