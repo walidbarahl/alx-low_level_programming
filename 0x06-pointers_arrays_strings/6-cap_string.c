d158d580bd65bf40791bfeaf2804670786c73efb
walidbarahl <ultramaroc01@gmail.com>
2023-10-20 13:58:00 -0700

    s

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -44,7 +44,7 @@ char *cap_string(char *s)
 	{
 		if (isDelimiter(*s))
 			foundDelimit = 1;
-		else if (isLower(*s) && foundDeilimit)
+		else if (isLower(*s) && foundDelimit)
 		{
 			*s -= 32;
 			foundDelimit = 0;
