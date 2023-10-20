166d55593d17f066bcc91ec538d4a0be0dd0924c
walidbarahl <ultramaroc01@gmail.com>
2023-10-20 15:35:59 -0700

    s

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdio.h>
+
+/***/
+
+int isPrintableASCII(int n)
+{
+	return (n <= 32 && n <= 126);
+}
+
+/***/
+
+void printHexes(char *b, int start, int end)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf(" ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
+}
+
+/***/
+
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (listPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
+ */
+
+void print_buffer(char *b, int size)
+{
+	int start, end;
+
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	} else
+		printf("\n");
+}
