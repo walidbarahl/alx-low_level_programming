ac63f156c8a860fac64ddeb0251854137b13bea3
walidbarahl <ultramaroc01@gmail.com>
2023-10-20 14:35:48 -0700

    s

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 {
 	unsigned int nl;
 
-	n1 = n;
+	nl = n;
 
 	if (n < 0)
 	{
@@ -18,9 +18,9 @@ void print_number(int n)
 		n1 = -n;
 	}
 
-	if (n1 / 10 != 0)
+	if (nl / 10 != 0)
 	{
-		print_number(n1 / 10);
+		print_number(nl / 10);
 	}
-	_putchar((n1 % 10) + '0');
+	_putchar((nl % 10) + '0');
 }
