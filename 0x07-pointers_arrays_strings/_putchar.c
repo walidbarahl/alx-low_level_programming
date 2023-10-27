322d9aeb572244b1851e83f69f2a81d4a7e9a2e3
walidbarahl <ultramaroc01@gmail.com>
2023-10-27 15:05:58 -0700

    s

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to tdout
- * @c: the character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: on success 1.
- * On error, -1 is returned, and erno is set appropriately.
+ * Return: On success 1.
+ * 	   On error. -1 is returned, and errno is set appropriatly.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
