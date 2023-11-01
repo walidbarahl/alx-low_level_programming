0e4faeda0411e02461b118a31a81649d81ebe41d
walidbarahl <ultramaroc01@gmail.com>
2023-11-01 16:12:05 -0700

    s

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
 		for (i = 0; i < 5; i++)
 		{
-			if (money >= cendts[i])
+			if (money >= cents[i])
 			{
 				leastcents += money / cents[i];
 				money = money % cents[i];
