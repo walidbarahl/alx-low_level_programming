4531608560a7cceffcc9632cee39a7fb0ffa7385
walidbarahl <ultramaroc01@gmail.com>
2023-11-06 12:02:37 -0800

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -67,6 +67,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) +1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
 	dog->name = _strcopy(dog->name, name);
 	dog->age = age;
 	dog->owner = _strcopy(dog->owner, owner);
