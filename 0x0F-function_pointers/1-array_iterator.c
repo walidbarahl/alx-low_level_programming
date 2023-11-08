aa17b2f2598d1d3e7229cbb9879abf0380ed8b86
walidbarahl <ultramaroc01@gmail.com>
2023-11-08 12:04:23 -0800

    s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,13 +5,18 @@
  * @array: the array size
  * @action: function pionter
  *
- * Return: void
+ * Return: nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end = array + size - 1;
+	int *end = array + size;
 
 	if (array && size && action)
+	{
 		while (array <= end)
-			action(*array++);
+		{
+			action(*array);
+			array++;
+		}
+	}
 }
