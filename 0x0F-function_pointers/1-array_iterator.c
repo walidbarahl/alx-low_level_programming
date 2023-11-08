9a8ceee6dcd684b851cf73890bfe4672c386d122
walidbarahl <ultramaroc01@gmail.com>
2023-11-08 12:08:35 -0800

    s

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,21 +2,17 @@
 
 /**
  * array_iterator - maps an array through a func pointer
- * @array: the array size
+ * @array: the int array
+ * @size: the array size
  * @action: function pionter
  *
  * Return: nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end = array + size;
+	int *end = array + size - 1;
 
 	if (array && size && action)
-	{
 		while (array <= end)
-		{
-			action(*array);
-			array++;
-		}
-	}
+			action(*array++);
 }
